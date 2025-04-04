3ba1e89402cb29fbbb154fc75a10ff7ac73d6aca
Agnes Kago <kagoagnes48@gmail.com>
2025-04-04 08:46:08 +0300

    Minor

--- hybrid_trainer/live_dashboard.py
+++ hybrid_trainer/live_dashboard.py
@@ -75,7 +75,7 @@ class LiveDashboard:
         )
         
         # Trace 2: Metaheuristic
-        for algo in ["de"]:  # , "pso", "aco"
+        for algo in ["de","pso", "aco"]:  # , "pso", "aco"
             self.fig.add_trace(go.Scattergl(
                 x=[], y=[],  mode='markers', visible=False, name=f'{algo.upper()} Agents',
                 marker=dict(size=8)), row=1, col=1
