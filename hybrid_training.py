db5c158ca9d22ad2e877a1a9b889a148f604f146
Agnes Kago <kagoagnes48@gmail.com>
2025-04-10 22:36:13 +0300

    Minor

--- hybrid_training.py
+++ hybrid_training.py
@@ -10,7 +10,7 @@ import time
 import numpy as np
 import pandas as pd
 from ray import tune
-from ray.tune.trial import Trial
+# from ray.tune.trial import Trial
 from typing import Dict
 from IPython.display import display
 from ray.rllib.algorithms.ppo import PPOConfig
@@ -166,7 +166,7 @@ class HybridTraining:
             #         algorithm= "PPO",
             #         metrics=metrics
             #     )
-            def on_step_end(self, *, result: dict, trial: Trial, **kwargs):
+            def on_step_end(self, *, result: dict, trial, **kwargs):
                 """Fixed signature with required parameters"""
                 # Extract metrics from result dict
                 metrics = {
